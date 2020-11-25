24235e56799755d23670d03bd2d77cfa1a10b4ab
Seanne Canoy <sacanoy16@gmail.com>
2020-11-25 20:44:08 +0800

    changes in assets for level 2

---
+++ Assets/Scripts/SpawnGameObjectsOnce.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnGameObjectsOnce : MonoBehaviour {
+
+	public GameObject spawnPrefab;
+
+	public float spawningSeconds = 5.0f;
+	
+	private float savedTime;
+	private float secondsBetweenSpawning;
+
+	// Use this for initialization
+	void Start () {
+		savedTime = Time.time;
+		secondsBetweenSpawning = spawningSeconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Time.time - savedTime >= secondsBetweenSpawning) && secondsBetweenSpawning != 0)// is it time to spawn again?
+		{
+			MakeThingToSpawn();
+			secondsBetweenSpawning = 0;
+		}	
+	}
+
+	void MakeThingToSpawn()
+	{
+		// create a new gameObject
+		GameObject clone = Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
+	}
+}
